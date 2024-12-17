49a3910d1cdab13781df2ee502514c27b132213b
DanHillesheim <79476558+DanHillesheim@users.noreply.github.com>
2024-12-17 16:26:33 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import WallpaperGenerator from './components/WallpaperGenerator'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <WallpaperGenerator />
+    </div>
+  )
+}
+
+export default App
